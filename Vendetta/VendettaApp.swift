9f68ea2e69be85e4d02a6ba5aa1d68bfbafe86de
Sam <82487541+example-git@users.noreply.github.com>
2023-06-28 05:48:40 -0400

    Initial Commit

---
+++ Vendetta/VendettaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendettaApp.swift
+//  Vendetta
+//
+//  Created by Sam  on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct VendettaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
